c5602d9c30e4d7a9aa7cabdaa6f3e5526112e663
Jan Wawszczak <janekw23@gmail.com>
2021-02-16 12:02:55 +0100

    Minor tweaks to optimized sequential implementation

--- PageRank/src/multiThreadedPageRankComputer.hpp
+++ PageRank/src/multiThreadedPageRankComputer.hpp
@@ -20,62 +20,25 @@
 // https://en.cppreference.com/w/cpp/container#Thread_safety
 // metody wstępnego haszhowania stron:
 // - przydzielenie każdemu wątkowi takiej samej liczby stron do shaszhowania
-//   (jeśli jakiś wątek dostanie większe strony, to inne możliwe że będą na niego czekać)
-//   (może przydzielenie robić losowo?)
+//   (jeśli jakiś wątek dostanie większe strony, to inne możliwe że będą na
+//   niego czekać) (może przydzielenie robić losowo?)
 // - pobieranie zadań z kolejki przez gotowe wątki
 //   (wymaga synchronizacji między wątkami)
 // można wcześniej przygotować struktury danych, każdą przez jeden wątek?
-// metody dostępu do poprzednich wartości rang stron, które linkują do danej strony:
+// metody dostępu do poprzednich wartości rang stron, które linkują do danej
+// strony:
 
-// todo hide it as private class member
-struct PageInfo {
-    // using struct should optimize caching and omit use of multiple maps
-
-    PageInfo() = default;
-    PageInfo(PageRank initialRank, size_t numLinks, bool isDangling)
-        : ranks { initialRank, initialRank }
-        , numLinks(numLinks)
-        , isDangling(isDangling)
-    {
-    }
-
-    /// Sets current rank to provided value. Returns the difference
-    /// between the previous rank (which it does not update!).
-    PageRank setCurrentRank(uint32_t iteration, PageRank newRank) noexcept
-    {
-        const bool odd = iteration % 2;
-        ranks[odd] = newRank;
-        return newRank - ranks[1 - odd];
-    }
-
-    /// Returns value of a single link from the page, calculated
-    /// for the previous value of PageRank.
-    PageRank getLinkValue(uint32_t iteration) const noexcept
-    {
-        return ranks[1 - iteration % 2] / numLinks;
-    }
-
-    PageRank getCurrentRank(uint32_t iteration)
-    {
-        return ranks[iteration % 2];
-    }
-
-    PageRank ranks[2]; // current and prev ranks, depending on iteration parity todo OK?
-    size_t numLinks; // todo original impl has uint32_t, why?
-    bool isDangling;
-};
 
 class MultiThreadedPageRankComputer : public PageRankComputer {
 public:
-    MultiThreadedPageRankComputer(uint32_t numThreadsArg)
-        : numThreads(numThreadsArg) {};
+    MultiThreadedPageRankComputer(uint32_t numThreadsArg) : numThreads(numThreadsArg) {};
 
     std::vector<PageIdAndRank>
-    computeForNetwork(Network const& network, double alpha, uint32_t iterations,
-        double tolerance) const
-    {
+    computeForNetwork(Network const &network, double alpha,
+                      uint32_t iterations, double tolerance) const {
+
         // todo parallelize
-        for (auto const& page : network.getPages()) {
+        for (auto const &page : network.getPages()) {
             page.generateId(network.getGenerator());
         }
 
@@ -83,14 +46,13 @@ public:
         std::unordered_map<PageId, std::vector<PageId>, PageIdHash> edges;
 
         size_t danglingCount = 0;
-        PageRank initialRank = 1.0 / network.getSize();
+        const PageRank initialRank = 1.0 / network.getSize();
 
         // todo parallelize
         for (auto page : network.getPages()) {
             size_t linksNum = page.getLinks().size();
             bool isDangling = (linksNum == 0);
             danglingCount += isDangling;
-
             pageHashMap[page.getId()] = PageInfo(initialRank, linksNum, isDangling);
         }
 
@@ -109,12 +71,13 @@ public:
             double prevDangleSum = dangleSum;
             double difference = 0;
 
-            for (auto& pageMapElem : pageHashMap) {
+            for (auto &pageMapElem : pageHashMap) {
                 PageId pageId = pageMapElem.first;
-                PageInfo& pageInfo = pageMapElem.second;
+                PageInfo &pageInfo = pageMapElem.second;
 
                 double danglingWeight = 1.0 / network.getSize();
-                PageRank newRank = prevDangleSum * alpha * danglingWeight + (1.0 - alpha) / network.getSize();
+                PageRank newRank = prevDangleSum * alpha * danglingWeight +
+                                   (1.0 - alpha) / network.getSize();
 
                 if (edges.count(pageId) > 0) {
                     for (auto link : edges[pageId]) {
@@ -131,11 +94,13 @@ public:
             if (difference < tolerance) {
                 std::vector<PageIdAndRank> result;
                 for (auto iter : pageHashMap) {
-                    result.push_back(PageIdAndRank(iter.first, iter.second.getCurrentRank(iteration)));
+                    result.push_back(
+                            PageIdAndRank(iter.first, iter.second.getCurrentRank(iteration)));
                 }
 
                 ASSERT(result.size() == network.getSize(),
-                       "Invalid result size=" << result.size() << ", for network" << network);
+                       "Invalid result size=" << result.size() << ", for network"
+                                              << network);
 
                 return result;
             }
@@ -144,13 +109,38 @@ public:
         ASSERT(false, "Not able to find result in iterations=" << iterations);
     }
 
-    std::string getName() const
-    {
+    std::string getName() const {
         return "MultiThreadedPageRankComputer[" + std::to_string(this->numThreads) + "]";
     }
 
 private:
     uint32_t numThreads;
+
+    struct PageInfo {
+        // using struct should optimize caching and omit use of multiple maps
+
+        PageInfo() = default;
+
+        PageInfo(PageRank initialRank, size_t numLinks, bool isDangling)
+                : ranks{initialRank, initialRank}, numLinks(numLinks), isDangling(isDangling) {
+        }
+
+        PageRank setCurrentRank(uint32_t iteration, PageRank newRank) noexcept {
+            const bool odd = iteration % 2;
+            ranks[odd] = newRank;
+            return newRank - ranks[1 - odd];
+        }
+
+        PageRank getLinkValue(uint32_t iteration) const {
+            return ranks[1 - iteration % 2] / numLinks;
+        }
+
+        PageRank getCurrentRank(uint32_t iteration) const noexcept { return ranks[iteration % 2]; }
+
+        PageRank ranks[2]; // current and prev ranks, depending on iteration parity
+        size_t numLinks; // todo original impl has uint32_t, why?
+        bool isDangling;
+    };
 };
 
 #endif /* SRC_MULTITHREADEDPAGERANKCOMPUTER_HPP_ */
--- PageRank/src/singleThreadedPageRankComputer.hpp
+++ PageRank/src/singleThreadedPageRankComputer.hpp
@@ -13,65 +13,74 @@ class SingleThreadedPageRankComputer : public PageRankComputer {
 public:
     SingleThreadedPageRankComputer() {};
 
-    std::vector<PageIdAndRank> computeForNetwork(Network const& network, double alpha, uint32_t iterations, double tolerance) const
-    {
-        std::unordered_map<PageId, PageRank, PageIdHash> pageHashMap;
-        for (auto const& page : network.getPages()) {
+    std::vector<PageIdAndRank>
+    computeForNetwork(Network const &network, double alpha, uint32_t iterations,
+                      double tolerance) const {
+        // todo parallelize
+        for (auto const &page : network.getPages()) {
             page.generateId(network.getGenerator());
-            pageHashMap[page.getId()] = 1.0 / network.getSize();
         }
 
-        std::unordered_map<PageId, uint32_t, PageIdHash> numLinks;
-        for (auto page : network.getPages()) {
-            numLinks[page.getId()] = page.getLinks().size();
-        }
-
-        std::unordered_set<PageId, PageIdHash> danglingNodes;
-        for (auto page : network.getPages()) {
-            if (page.getLinks().size() == 0) {
-                danglingNodes.insert(page.getId());
-            }
-        }
-
+        std::unordered_map<PageId, PageInfo, PageIdHash> pageHashMap;
         std::unordered_map<PageId, std::vector<PageId>, PageIdHash> edges;
+
+        size_t danglingCount = 0;
+        const PageRank initialRank = 1.0 / network.getSize();
+
+        // todo parallelize
+        for (auto page : network.getPages()) {
+            size_t linksNum = page.getLinks().size();
+            bool isDangling = (linksNum == 0);
+            danglingCount += isDangling;
+            pageHashMap[page.getId()] = PageInfo(initialRank, linksNum, isDangling);
+        }
+
+        // todo parallelize
+        // Prepare this structure using synchronized hashmap access (prob better)
+        // or keep this data in the struct and update online?
         for (auto page : network.getPages()) {
             for (auto link : page.getLinks()) {
                 edges[link].push_back(page.getId());
             }
         }
 
-        for (uint32_t i = 0; i < iterations; ++i) {
-            std::unordered_map<PageId, PageRank, PageIdHash> previousPageHashMap = pageHashMap;
+        double dangleSum = initialRank * danglingCount;
 
-            double dangleSum = 0;
-            for (auto danglingNode : danglingNodes) {
-                dangleSum += previousPageHashMap[danglingNode];
-            }
-            dangleSum = dangleSum * alpha;
-
+        for (uint32_t iteration = 0; iteration < iterations; ++iteration) {
+            double prevDangleSum = dangleSum;
             double difference = 0;
-            for (auto& pageMapElem : pageHashMap) {
+
+            for (auto &pageMapElem : pageHashMap) {
                 PageId pageId = pageMapElem.first;
+                PageInfo &pageInfo = pageMapElem.second;
 
                 double danglingWeight = 1.0 / network.getSize();
-                pageMapElem.second = dangleSum * danglingWeight + (1.0 - alpha) / network.getSize();
+                PageRank newRank = prevDangleSum * alpha * danglingWeight +
+                                   (1.0 - alpha) / network.getSize();
 
                 if (edges.count(pageId) > 0) {
                     for (auto link : edges[pageId]) {
-                        pageMapElem.second += alpha * previousPageHashMap[link] / numLinks[link];
+                        newRank += alpha * pageHashMap[link].getLinkValue(iteration);
                     }
                 }
-                difference += std::abs(previousPageHashMap[pageId] - pageHashMap[pageId]);
-            }
 
-            std::vector<PageIdAndRank> result;
-            for (auto iter : pageHashMap) {
-                result.push_back(PageIdAndRank(iter.first, iter.second));
-            }
+                PageRank rankDifference = pageInfo.setCurrentRank(iteration, newRank);
+                dangleSum += rankDifference * pageInfo.isDangling;
 
-            ASSERT(result.size() == network.getSize(), "Invalid result size=" << result.size() << ", for network" << network);
+                difference += std::abs(rankDifference);
+            }
 
             if (difference < tolerance) {
+                std::vector<PageIdAndRank> result;
+                for (auto iter : pageHashMap) {
+                    result.push_back(
+                            PageIdAndRank(iter.first, iter.second.getCurrentRank(iteration)));
+                }
+
+                ASSERT(result.size() == network.getSize(),
+                       "Invalid result size=" << result.size() << ", for network"
+                                              << network);
+
                 return result;
             }
         }
@@ -79,10 +88,34 @@ public:
         ASSERT(false, "Not able to find result in iterations=" << iterations);
     }
 
-    std::string getName() const
-    {
+    std::string getName() const {
         return "SingleThreadedPageRankComputer";
     }
+
+private:
+    struct PageInfo {
+        PageInfo() = default;
+
+        PageInfo(PageRank initialRank, size_t numLinks, bool isDangling)
+                : ranks{initialRank, initialRank}, numLinks(numLinks), isDangling(isDangling) {
+        }
+
+        PageRank setCurrentRank(uint32_t iteration, PageRank newRank) noexcept {
+            const bool odd = iteration % 2;
+            ranks[odd] = newRank;
+            return newRank - ranks[1 - odd];
+        }
+
+        PageRank getLinkValue(uint32_t iteration) const {
+            return ranks[1 - iteration % 2] / numLinks;
+        }
+
+        PageRank getCurrentRank(uint32_t iteration) const noexcept { return ranks[iteration % 2]; }
+
+        PageRank ranks[2];
+        size_t numLinks; // todo original impl has uint32_t, why?
+        bool isDangling;
+    };
 };
 
 #endif /* SRC_SINGLETHREADEDPAGERANKCOMPUTER_HPP_ */

1d326df8bae47193254f90f919a4ff07f960a501
Jan Wawszczak <janekw23@gmail.com>
2021-02-20 15:42:49 +0100

    Moved blocks of code to separate functions

--- PageRank/src/singleThreadedPageRankComputer.hpp
+++ PageRank/src/singleThreadedPageRankComputer.hpp
@@ -18,70 +18,27 @@ public:
         double tolerance) const
     {
         // todo parallelize
-        for (auto const& page : network.getPages()) {
-            page.generateId(network.getGenerator());
-        }
+        generatePageIds(network);
 
-        std::unordered_map<PageId, PageInfo, PageIdHash> pageHashMap;
-        std::unordered_map<PageId, std::vector<PageId>, PageIdHash> edges;
+        PageMap pageHashMap;
+        EdgeMap edges;
+
+        initEdges(network, edges);
 
         size_t danglingCount = 0;
         const PageRank initialRank = 1.0 / network.getSize();
-
-        // todo parallelize
-        for (auto const& page : network.getPages()) {
-            size_t linksNum = page.getLinks().size();
-            bool isDangling = (linksNum == 0);
-            danglingCount += isDangling;
-            pageHashMap[page.getId()] = PageInfo(initialRank, linksNum, isDangling);
-        }
-
-        // todo parallelize
-        // Prepare this structure using synchronized hashmap access (prob better)
-        // or keep this data in the struct and update online?
-        for (auto const& page : network.getPages()) {
-            for (auto const& link : page.getLinks()) {
-                edges[link].push_back(page.getId());
-            }
-        }
-
+        initPages(network, pageHashMap, initialRank, danglingCount);
         double dangleSum = initialRank * danglingCount;
 
         for (uint32_t iteration = 0; iteration < iterations; ++iteration) {
-            double prevDangleSum = dangleSum;
-            double difference = 0;
+            auto changes = updateRanks(pageHashMap, edges,
+                                       network.getSize(), dangleSum, alpha, iteration);
 
-            for (auto& pageMapElem : pageHashMap) {
-                PageId const& pageId = pageMapElem.first;
-                PageInfo& pageInfo = pageMapElem.second;
-
-                double danglingWeight = 1.0 / network.getSize();
-                PageRank newRank = prevDangleSum * alpha * danglingWeight + (1.0 - alpha) / network.getSize();
-
-                if (edges.count(pageId) > 0) {
-                    for (auto const& link : edges[pageId]) {
-                        newRank += alpha * pageHashMap[link].getLinkValue(iteration);
-                    }
-                }
-
-                PageRank rankDifference = pageInfo.setCurrentRank(iteration, newRank);
-                dangleSum += rankDifference * pageInfo.isDangling;
-
-                difference += std::abs(rankDifference);
-            }
+            auto difference = changes.first;
+            dangleSum += changes.second;
 
             if (difference < tolerance) {
-                std::vector<PageIdAndRank> result;
-                for (auto iter : pageHashMap) {
-                    result.push_back(
-                        PageIdAndRank(iter.first, iter.second.getCurrentRank(iteration)));
-                }
-
-                ASSERT(result.size() == network.getSize(),
-                    "Invalid result size=" << result.size() << ", for network"
-                                           << network);
-
-                return result;
+                return generateResult(pageHashMap, network, iteration);
             }
         }
 
@@ -122,6 +79,78 @@ private:
         size_t numLinks; // todo original impl has uint32_t, why?
         bool isDangling;
     };
+
+    using PageMap = std::unordered_map<PageId, PageInfo, PageIdHash>;
+    using EdgeMap = std::unordered_map<PageId, std::vector<PageId>, PageIdHash>;
+
+    static void generatePageIds(Network const& network) {
+        for (auto const& page : network.getPages()) {
+            page.generateId(network.getGenerator());
+        }
+    }
+
+    static void initPages(Network const& network, PageMap& pageHashMap,
+                          PageRank initialRank, size_t& danglingCount) {
+        danglingCount = 0;
+
+        for (auto const& page : network.getPages()) {
+            size_t linksNum = page.getLinks().size();
+            bool isDangling = (linksNum == 0);
+            danglingCount += isDangling;
+            pageHashMap[page.getId()] = PageInfo(initialRank, linksNum, isDangling);
+        }
+    }
+
+    static void initEdges(Network const& network, EdgeMap &edges) {
+        for (auto const& page : network.getPages()) {
+            for (auto const& link : page.getLinks()) {
+                edges[link].push_back(page.getId());
+            }
+        }
+    }
+
+    static std::pair<double, double>
+    updateRanks(PageMap& pageHashMap, EdgeMap& edges,
+                size_t networkSize, const double dangleSum, const double alpha, const uint32_t iteration){
+
+        double dangleSumChange = 0;
+        double pageRankCumulativeChange = 0;
+
+        for (auto& pageMapElem : pageHashMap) {
+            PageId const& pageId = pageMapElem.first;
+            PageInfo& pageInfo = pageMapElem.second;
+
+            double danglingWeight = 1.0 / networkSize;
+            PageRank newRank = dangleSum * alpha * danglingWeight + (1.0 - alpha) / networkSize;
+
+            if (edges.count(pageId) > 0) {
+                for (auto const& link : edges[pageId]) {
+                    newRank += alpha * pageHashMap[link].getLinkValue(iteration);
+                }
+            }
+
+            PageRank rankDifference = pageInfo.setCurrentRank(iteration, newRank);
+            dangleSumChange += rankDifference * pageInfo.isDangling;
+            pageRankCumulativeChange += std::abs(rankDifference);
+        }
+
+        return {pageRankCumulativeChange, dangleSumChange};
+    }
+
+    static std::vector<PageIdAndRank>
+    generateResult(PageMap const& pageHashMap, Network const& network, uint32_t iteration)
+    {
+        std::vector<PageIdAndRank> result;
+
+        for (auto iter : pageHashMap) {
+            result.push_back(PageIdAndRank(iter.first, iter.second.getCurrentRank(iteration)));
+        }
+
+        ASSERT(result.size() == network.getSize(),
+               "Invalid changes size=" << result.size() << ", for network" << network);
+
+        return result;
+    }
 };
 
 #endif /* SRC_SINGLETHREADEDPAGERANKCOMPUTER_HPP_ */
